6d72e7cae7cf2a7e3e4448880f196bf781724495
Devin Xie <devinxie418@gmail.com>
2024-12-23 19:15:25 -0500

    implemented logic and arithmetic

--- index.js
+++ index.js
@@ -1,47 +1,136 @@
 // Display
 const display = document.querySelector("#display");
-const p = document.querySelector("#display p");
-p.style.cssText = "color: #FFFFFF; font-size: 32px; margin: auto 10px 10px;"
+const p = document.querySelector("#display .main");
+p.style.cssText = "color: #FFFFFF; font-size: 32px; margin: 0px 10px 10px; height: 40px;";
+
+
+
+// Functions
+const addToDisplay = (newContent) => {
+    if(display.clientWidth - p.clientWidth > 40) {
+        p.textContent += newContent;
+    }
+}
+
+const getFormat = () => {
+    const operators = ["÷", "×", "-", "+", "%"];
+    const symbol = operators.filter(op => p.textContent.includes(op));
+    const index = p.textContent.indexOf(symbol);
+
+    if(index == 0) {
+        return [];
+    }
+
+    const left = p.textContent.slice(0,index);
+    const right = p.textContent.slice(index + 1);
+
+    return [left, symbol, right];
+}
+
+const insertDecimal = () => {
+    const format = getFormat();
+
+    if((!format[2].includes(".")) && 
+    (/\d/.test(p.textContent.slice(-1)))) {
+        addToDisplay(".");
+    }
+}
+
+const calculate = (num1, num2, operator) => {
+    let result;
+    switch(operator[0]) {
+        case "%":
+            result = num1%num2;
+            return Math.round(result*1000)/1000;
+        case "÷":
+            result = num1/num2;
+            return Math.round(result*1000)/1000;
+        case "-":
+            result = num1-num2;
+            return Math.round(result*1000)/1000;
+        case "+":
+            result = num1+num2;
+            return Math.round(result*1000)/1000;
+        case "×":
+            result = num1*num2;
+            return Math.round(result*1000)/1000;
+    }
+}
+
+const checkOperators = (operator) => {
+    const operators = ["÷", "×", "-", "+", "%"];
+    
+    if(operator != "=") {
+        if((p.textContent.replace(".", "") != "") && 
+        (!operators.some(op => p.textContent.includes(op)))) {
+            addToDisplay(operator);
+        }
+    }
+    else {
+        if((p.textContent.replace(".", "") != "") && 
+        (operators.some(op => p.textContent.includes(op))) && 
+        (operators.every(op => p.textContent.slice(-1) != op))) {
+            history.textContent = p.textContent;
+            
+            const format = getFormat();
+            
+            const num1 = Number(format[0]);
+            const num2 = Number(format[2]);
+            
+            p.textContent = "";
+            addToDisplay(calculate(num1, num2, format[1]));
+        }
+    }
+}
+
+
 
 // Digits
 const digits = document.querySelectorAll(".digit");
 
-function insertDecimal(char) {
-    if(!p.textContent.includes(char)) {
-        p.textContent += char;
-    }
-}
-
 digits.forEach((digit) => {
     digit.addEventListener("click", () => {
-        if(display.clientWidth - p.clientWidth > 40) {
-            if(digit.textContent == '.') {
-                insertDecimal(digit.textContent);
+        if(digit.textContent == ".") {
+            insertDecimal();
+        }
+        else if(digit.textContent == "0") {
+            const format = getFormat();
+            
+            if((format.length == 0 && p.textContent != "0") || 
+            (format.length > 0 && format[2] != "0")) {
+                addToDisplay(digit.textContent);
+            }
+        }
+        else {
+            const format = getFormat();
+            console.log(format);
+
+            if((format.length == 0 && p.textContent == "0") || 
+            (format.length > 0 && format[2] == "0")) {
+                p.textContent= p.textContent.slice(0,-1) + digit.textContent;
             }
             else {
-                p.textContent += digit.textContent;
+                addToDisplay(digit.textContent);
             }
         }
     });
 });
 
 
+
 // Misc
 const miscellaneous = document.querySelectorAll(".misc");
-
-function modulo() {
-    if(p.textContent.replace('.', '') != '') {
-
-    }
-}
 
 miscellaneous.forEach((misc) => {
     misc.addEventListener("click", () => {
         if(misc.textContent == "AC") {
-            p.textContent = "";
+            p.textContent = "0";
         }
         else if(misc.textContent == "C") {
             p.textContent = p.textContent.slice(0,-1);
+            if(p.textContent == "") {
+                p.textContent = "0"
+            }
         }
         else {
             checkOperators(misc.textContent);
@@ -50,22 +139,11 @@ miscellaneous.forEach((misc) => {
 })
 
 
+
 // Operators
 const operators = document.querySelectorAll(".operator");
-
-function checkOperators(operator) {
-    if(operator != '=') {
-        const operators = ['÷', '×', '-', '+', '%'];
-
-        if((p.textContent.replace('.', '') != "") && (!operators.some(op => p.textContent.includes(op)))) {
-            p.textContent += operator;
-        }
-    }
-    // if clicked '='
-    else {
-
-    }
-}
+const history = document.querySelector("#display .history");
+history.style.cssText = "color: lightgray; font-size: 16px; margin: auto 10px 5px;";
 
 operators.forEach((operator) => {
     operator.addEventListener("click", () => {
